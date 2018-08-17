1f173bcfaaa4e823382db188ec4b303eb925573e
Greg Turmel <gregturmel@gmail.com>
2018-08-17 07:12:07 -0400

    week one mongo u course

--- WeekOne/all_together_now/app.js
+++ WeekOne/all_together_now/app.js
@@ -1,34 +1,50 @@
 const express = require('express'),
-    app = express(),
-    engines = require('consolidate'),
-    MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+  app = express(),
+  engines = require('consolidate'),
+  MongoClient = require('mongodb').MongoClient,
+  assert = require('assert'),
+  bodyParser = require('body-parser');
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
 
-    assert.equal(null, err);
-    console.log("Successfully connected to MongoDB.");
+  assert.equal(null, err);
+  console.log("Successfully connected to MongoDB.");
 
-    app.get('/', function(req, res){
+  app.get('/', function(req, res){
 
-        db.collection('movies').find({}).toArray(function(err, docs) {
-            res.render('movies', { 'movies': docs } );
-        });
-
+    db.collection('movies').find({}).toArray(function(err, docs) {
+      res.render('movies', { 'movies': docs } );
     });
 
-    app.use(function(req, res){
-        res.sendStatus(404);
+  });
+
+  app.get('/form', (req, res) => {
+    res.render('form')
+  });
+
+  app.post('/post-movie', (req, response, next) => {
+    const newMovie = req.body;
+    db.collection('movies').insertOne(newMovie)
+      .then((req, res) => {
+
+        response.render('result');
     });
-    
-    const server = app.listen(3000, function() {
-        const port = server.address().port;
-        console.log('Express server listening on port %s.', port);
-    });
+  });
+
+  app.use(function(req, res){
+    res.sendStatus(404);
+  });
+
+  const server = app.listen(3000, function() {
+    const port = server.address().port;
+    console.log('Express server listening on port %s.', port);
+  });
 
 });
 
